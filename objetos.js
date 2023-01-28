e7ffb85b8e6057aafa3c0f1227cf941b8868398f
Renato Paracone <renato.paracone@hotmail.com>
2023-01-28 00:59:17 -0300

    cambios

--- objetos.js
+++ objetos.js
@@ -1,85 +1,70 @@
-
-/*
-
-Javascript 
-segunda
-entrega
-
-*/
-
-
-
+alert('Productos en stock: Darth vader, llavero star wars, torre eiffel, yoda, avion, auto, camello, arbol')
 function Producto(nombre, color, precio)
-    {
-        this.nombre = nombre;
-        this.color = color; 
-        this.precio = precio;
-    }
+{
+    this.nombre = nombre;
+    this.color = color; 
+    this.precio = precio;
+}
 
-const producto1 = new Producto ('DARTH VADER', 'Negro', Number(1000))
-const producto2 = new Producto ('LLAVERO STAR WARS', 'Negro y rojo', Number(500))
-const producto3 = new Producto ('TORRE EIFFEL', 'Gris', Number(2000))
 
-// Calculo de gastos totales a pagar //
-function precioapagar()
+const producto1 = new Producto (['DARTH VADER','DARTHH VADER', 'DAR VADER','DARS VADER'], 'Negro', 1000);
+const producto2 = new Producto (['LLAVERO STAR WARS', 'llavero star wors', 'llavero de star wars', 'star wars llavero'], 'Negro y rojo', 500);
+const producto3 = new Producto (['Torre Eiffel', 'torre eifel', 'torre ifel', 'torre eifel'], 'Gris', 2000);
+const producto4 = new Producto ('YODA', 'Verde', 1500);
+const producto5 = new Producto (['Auto','car'], 'Azul', 400);
+const producto6 = new Producto (['Avion','abion','plane'], 'Blanco y celeste', 1000);
+const producto7 = new Producto ('CAMELLO', 'Naranja', 4000);
+const producto8 = new Producto (['Arbol','arrbol','arbol'], 'Verde', 5000);
+
+
+let precioTotal = Number(0)
+
+function gastototal()
 { 
-let precioDarthVader = Number("1000")
-let precioEiffel = Number("2000")
-let PrecioLlavero = Number("500")
-let precioTotal = Number("0")
-let PrecioTotal2 = Number("0")
-let precioTota3 = Number("0")
-for (let a=0; a<3; a++) 
-{
-    productos = prompt("Elija el producto que quiera").toUpperCase()
-    if (productos == producto1.nombre)
+    for(let i = 0; i<2; i++)
     {
-        precioTotal += (precioDarthVader) 
+    productos = prompt("Elija el producto que quiera y calcule su gasto").toUpperCase()  
+        if (productos == (producto1.nombre[0])) 
+    {
+        precioTotal += (producto1.precio)
     
     }
-        else if (productos == producto2.nombre)
+        else if (productos == producto2.nombre[0])
     {
-        PrecioTotal2 += (precioEiffel) 
+        precioTotal += (producto2.precio) 
     
     }
-        else if (nombre == producto3.nombre)
+    else if (productos == producto3.nombre)
     {
-        precioTota3 += (PrecioLlavero) 
+        precioTotal += (producto3.precio) 
     }
-
+        else if (productos == producto4.nombre)
+    {
+        precioTotal += (producto4.precio) 
+    }
+        else if (productos == producto5.nombre)
+    {
+        precioTotal += (producto5.precio) 
+    }
+        else if (productos == producto6.nombre)
+    {
+        precioTotal += (producto6.precio) 
+    }
+        else if (productos == producto7.nombre)
+    {
+        precioTotal += (producto7.precio) 
+    }
+        else if (productos == producto8.nombre)
+    {
+        precioTotal += (producto8.precio) 
+    }
+        else 
+    {    
+    productos = prompt("Elija el producto que quiera y calcule su gasto").toUpperCase()
+    }
 }
-let suma = (precioTotal+ PrecioTotal2 + precioTota3)
-console.log(precioTotal+ PrecioTotal2 + precioTota3) 
-console.log(suma)
-alert("El gasto total es de: $" + suma)
 }
-
-precioapagar()
-
-
-
-
-
-
-todosLosproductos = producto1, producto2, producto3
-for(let i = 0; i<5; i++)
-{
-array = prompt('Elija un producto. recomendaciones: darth vader, llavero star wars, torre eiffel').toUpperCase
-
-const arrayproductos =
-[
-producto1, producto2, producto3
-]
-
-if (array == todosLosproductos)
-    alert('El producto esta dentro de los indicados')
-else 
-    alert('El producto no esta dentro de los indicados')
-
-    array = prompt('Elija un producto. recomendaciones: darth vader, llavero star wars, torre eiffel').toUpperCase
-}
-
-
-
+gastototal()
+alert("El gasto total es de: $" + precioTotal)
 
 
